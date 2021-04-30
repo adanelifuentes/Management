540f9d804b8c49073b601fceb93475e1962284e0
Adán Eli Fuentes <adamsp1000@live.com>
2021-04-29 20:39:02 -0500

    Agregar archivos de proyecto.

---
+++ Management/frmLogin.vb
@@ -0,0 +1,8 @@
+﻿Public Class frmLogin
+    Private Sub frmLogin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    End Sub
+End Class
